71b378d7c694211de30a9c25c758df7452eff071
Emanuel-Lima7 <149845211+Emanuel-Lima7@users.noreply.github.com>
2023-11-22 03:12:26 -0300

    Add files via upload

---
+++ Site_projeto/admin/lista-usuarios.php
@@ -0,0 +1,35 @@
+<h1>Lista de Usuários</h1>
+<?php
+
+    $sql = "SELECT * FROM usuarios";
+
+    $res = $conn->query($sql);
+
+    $qtd = $res->num_rows;
+
+    if($qtd > 0){
+        print"<table class='table table-hover table-bordered'>";
+        print "<tr>";
+            print "<th>id</th>";
+            print "<th>Nome</th>";
+            print "<th>E-mail</th>";
+            print "<th>Ações</th>";
+            print "</tr>";
+        while($row = $res->fetch_object()){
+            print "<tr>";
+            print "<td>".$row->id."</td>";
+            print "<td>".$row->nome."</td>";
+            print "<td>".$row->email."</td>";
+            print   "<td>
+                        <button onclick=\"location.href='?page=editar&id=".$row->id."';\" class='btn btn-success'>Editar</button>
+
+                        <button onclick=\"if(confirm('Deseja excluir permanentemente?')){location.href='?page=salvar&acao=excluir&id=".$row->id."'}else{false;}\" class='btn btn-danger'>Excluir</button>
+                    </td>";
+            print "</tr>";
+        }
+        print "</table>";
+    }else{
+        print "<p class='alert alert-dander'>Sem resultados...</p>";
+    }
+
+?>
